7f89c7bb4db95650a9929c679c8d834576ec2971
Sumit Malhotra <Sumitmalhotra2002@gmail.com>
2025-08-17 13:33:05 +0530

    FrontEnd Update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -22,6 +21,7 @@ import Security from "./pages/Security";
 import Search from "./pages/Search";
 import TimeTracking from "./pages/TimeTracking";
 import NotFound from "./pages/NotFound";
+import ClientDashboard from "./pages/ClientDashboard";
 
 const queryClient = new QueryClient();
 
@@ -49,6 +49,7 @@ const App = () => (
             <Route path="/security" element={<Security />} />
             <Route path="/search" element={<Search />} />
             <Route path="/time-tracking" element={<TimeTracking />} />
+            <Route path="/dashboard/client" element={<ClientDashboard />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
